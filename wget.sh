652904e2e90fb220a6f2f39803386162a141447d
Troy Will <troydwill@gmail.com>
2012-05-24 15:16:18 -0700

    parenthesis

--- wget.sh
+++ wget.sh
@@ -1,2 +1,2 @@
-wget --no-clobber http://www.foscam.es/descarga/IP_Camera_CGI_(SDK).pdf
+wget --no-clobber 'http://www.foscam.es/descarga/IP_Camera_CGI_(SDK).pdf'
 wget --no-clobber http://www.foscam.es/descarga/Foscam_H.264_CGI_SDK_package.zip
