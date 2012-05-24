507f4431441e09ecce631f89eeb561e79caa95c5
Troy Will <troydwill@gmail.com>
2012-05-24 14:57:05 -0700

    FoscamFI8910W.pm: perltidy

--- Control/FoscamFI8910W.pm
+++ Control/FoscamFI8910W.pm
@@ -2,7 +2,7 @@
 #
 # ZoneMinder Foscam FI8910W IP Control Protocol Module
 # Copyright (C) 2012  Troy Will
-# from the Zoneminder PanasonicIP.pm by Philip Coombes, Copyright (C) 2001-2008  
+# from the Zoneminder PanasonicIP.pm by Philip Coombes, Copyright (C) 2001-2008
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -47,11 +47,10 @@ use ZoneMinder::Config qw(:all);
 
 use Time::HiRes qw( usleep );
 
-sub new
-{
+sub new {
     my $class = shift;
-    my $id = shift;
-    my $self = ZoneMinder::Control->new( $id );
+    my $id    = shift;
+    my $self  = ZoneMinder::Control->new($id);
     bless( $self, $class );
     srand( time() );
     return $self;
@@ -59,193 +58,172 @@ sub new
 
 our $AUTOLOAD;
 
-sub AUTOLOAD
-{
-    my $self = shift;
-    my $class = ref($self) || croak( "$self not object" );
-    my $name = $AUTOLOAD;
+sub AUTOLOAD {
+    my $self  = shift;
+    my $class = ref($self) || croak("$self not object");
+    my $name  = $AUTOLOAD;
     $name =~ s/.*://;
-    if ( exists($self->{$name}) )
-    {
-        return( $self->{$name} );
+    if ( exists( $self->{$name} ) ) {
+        return ( $self->{$name} );
     }
-    Fatal( "Can't access $name member of object of class $class" );
+    Fatal("Can't access $name member of object of class $class");
 }
 
-sub open
-{
+sub open {
     my $self = shift;
 
     $self->loadMonitor();
 
     use LWP::UserAgent;
     $self->{ua} = LWP::UserAgent->new;
-    $self->{ua}->agent( "ZoneMinder Control Agent/".ZM_VERSION );
+    $self->{ua}->agent( "ZoneMinder Control Agent/" . ZM_VERSION );
 
     $self->{state} = 'open';
 }
 
-sub close
-{
+sub close {
     my $self = shift;
     $self->{state} = 'closed';
 }
 
-sub printMsg
-{
-    my $self = shift;
-    my $msg = shift;
+sub printMsg {
+    my $self    = shift;
+    my $msg     = shift;
     my $msg_len = length($msg);
 
-    Debug( $msg."[".$msg_len."]" );
+    Debug( $msg . "[" . $msg_len . "]" );
 }
 
-sub sendCmd
-{
+sub sendCmd {
     my $self = shift;
-    my $cmd = shift;
+    my $cmd  = shift;
 
     my $result = undef;
 
     printMsg( $cmd, "Tx" );
 
     my $camera_address = $self->{Monitor}->{ControlAddress};
-    my $credentials = $self->{Monitor}->{ControlDevice};
+    my $credentials    = $self->{Monitor}->{ControlDevice};
     my $req =
-      HTTP::Request->new( GET => "http://$camera_address/$cmd".'&'."$credentials" );
+      HTTP::Request->new(
+        GET => "http://$camera_address/$cmd" . '&' . "$credentials" );
 
     my $res = $self->{ua}->request($req);
 
-    if ( $res->is_success )
-    {
+    if ( $res->is_success ) {
         $result = !undef;
     }
-    else
-    {
-        Error( "Error check failed: '".$res->status_line()."'" );
+    else {
+        Error( "Error check failed: '" . $res->status_line() . "'" );
     }
 
-    return( $result );
+    return ($result);
 }
 
-sub cameraReset
-{
+sub cameraReset {
     my $self = shift;
-    Debug( "Camera Reset" );
+    Debug("Camera Reset");
     my $cmd = "nphRestart?PAGE=Restart&Restart=OK";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub moveConUp
-{
+sub moveConUp {
     my $self = shift;
-    Debug( "Move Up" );
+    Debug("Move Up");
     my $cmd = "decoder_control.cgi?command=0";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub moveStop
-{
-	my $self = shift;
-	Debug( "Move Stop" );
-	my $cmd = "decoder_control.cgi?command=1";
-	$self->sendCmd( $cmd );
-}
-
-sub moveConDown
-{
+sub moveStop {
     my $self = shift;
-    Debug( "Move Down" );
+    Debug("Move Stop");
+    my $cmd = "decoder_control.cgi?command=1";
+    $self->sendCmd($cmd);
+}
+
+sub moveConDown {
+    my $self = shift;
+    Debug("Move Down");
     my $cmd = "decoder_control.cgi?command=2";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub moveConLeft
-{
+sub moveConLeft {
     my $self = shift;
-    Debug( "Move Left" );
+    Debug("Move Left");
     my $cmd = "decoder_control.cgi?command=6";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub moveConRight
-{
+sub moveConRight {
     my $self = shift;
-    Debug( "Move Right" );
+    Debug("Move Right");
     my $cmd = "decoder_control.cgi?command=4";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub moveConUpRight
-{
-	my $self = shift;
-	Debug( "Move Diagonally Up Right" );
-	my $cmd = "decoder_control.cgi?command=90";
-	$self->sendCmd( $cmd );
-}
-
-sub moveConDownRight
-{
-	my $self = shift;
-	Debug( "Move Diagonally Down Right" );
-	my $cmd = "decoder_control.cgi?command=92";
-	$self->sendCmd( $cmd );
-}
-
-sub moveConUpLeft
-{
-	my $self = shift;
-	Debug( "Move Diagonally Up Left" );
-	my $cmd = "decoder_control.cgi?command=91";
-	$self->sendCmd( $cmd );
-}
-
-sub moveConDownLeft
-{
-	my $self = shift;
-	Debug( "Move Diagonally Down Left" );
-	my $cmd = "decoder_control.cgi?command=93";
-	$self->sendCmd( $cmd );
-}
-
-sub presetClear
-{
+sub moveConUpRight {
     my $self = shift;
+    Debug("Move Diagonally Up Right");
+    my $cmd = "decoder_control.cgi?command=90";
+    $self->sendCmd($cmd);
+}
+
+sub moveConDownRight {
+    my $self = shift;
+    Debug("Move Diagonally Down Right");
+    my $cmd = "decoder_control.cgi?command=92";
+    $self->sendCmd($cmd);
+}
+
+sub moveConUpLeft {
+    my $self = shift;
+    Debug("Move Diagonally Up Left");
+    my $cmd = "decoder_control.cgi?command=91";
+    $self->sendCmd($cmd);
+}
+
+sub moveConDownLeft {
+    my $self = shift;
+    Debug("Move Diagonally Down Left");
+    my $cmd = "decoder_control.cgi?command=93";
+    $self->sendCmd($cmd);
+}
+
+sub presetClear {
+    my $self   = shift;
     my $params = shift;
     my $preset = $self->getParam( $params, 'preset' );
-    Debug( "Clear Preset $preset" );
+    Debug("Clear Preset $preset");
     my $cmd = "nphPresetNameCheck?Data=$preset";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub presetSet
-{
-    my $self = shift;
-    my $params = shift;
-    my $preset = $self->getParam( $params, 'preset' );
-    my $presetCmd = 30 + ($preset*2);
-    Debug( "Set Preset $preset: $presetCmd" );
+sub presetSet {
+    my $self      = shift;
+    my $params    = shift;
+    my $preset    = $self->getParam( $params, 'preset' );
+    my $presetCmd = 30 + ( $preset * 2 );
+    Debug("Set Preset $preset: $presetCmd");
     my $cmd = "decoder_control.cgi?command=$presetCmd";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub presetGoto
-{
-    my $self = shift;
-    my $params = shift;
-    my $preset = $self->getParam( $params, 'preset' );
-    my $presetCmd = 31 + ($preset*2);
-    Debug( "Goto Preset $preset: $presetCmd" );
+sub presetGoto {
+    my $self      = shift;
+    my $params    = shift;
+    my $preset    = $self->getParam( $params, 'preset' );
+    my $presetCmd = 31 + ( $preset * 2 );
+    Debug("Goto Preset $preset: $presetCmd");
     my $cmd = "decoder_control.cgi?command=$presetCmd";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
-sub presetHome
-{
+sub presetHome {
     my $self = shift;
-    Debug( "Home Preset" );
+    Debug("Home Preset");
     my $cmd = "decoder_control.cgi?command=25";
-    $self->sendCmd( $cmd );
+    $self->sendCmd($cmd);
 }
 
 1;
